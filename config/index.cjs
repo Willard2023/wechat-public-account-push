005eb88175d5d69f631c61a1c5f7cfb96a74bbff
Willard <110920421+Willard2023@users.noreply.github.com>
2024-04-22 22:44:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oyTvA6WpdyvyoYQsZ6wS5UoNNoVM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'LgiPur9L9xSz6gxPuwquOCvpxi41JC0-ezxg77dRm1c',
+      useTemplateId: 'DJZAdql2DAmgk4QjLc_j1ezdhPxHwCKppmtal0DIdvg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'LgiPur9L9xSz6gxPuwquOCvpxi41JC0-ezxg77dRm1c',
+  CALLBACK_TEMPLATE_ID: 'DJZAdql2DAmgk4QjLc_j1ezdhPxHwCKppmtal0DIdvg',
 
   CALLBACK_USERS: [
     {
