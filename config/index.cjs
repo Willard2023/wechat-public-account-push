c2f7addb32935a91241e541d21c7a377760a8f01
Willard <110920421+Willard2023@users.noreply.github.com>
2024-04-22 22:51:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -27,24 +27,24 @@ const USER_CONFIG = {
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
+        // // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        // {
+        //   type: '*生日', name: '梦倩宝宝', year: '2023', date: '01-26',
+        // },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '梦倩宝宝', year: '2001', date: '07-06',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '恋爱纪念日', year: '2024', date: '02-29',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        // 相识的日子
+        { keyword: 'love_day', date: '2023-12-26' },
+        // 在一起纪念日
+        { keyword: 'marry_day', date: '2024-02-29' },
       ],
     },
   ],
